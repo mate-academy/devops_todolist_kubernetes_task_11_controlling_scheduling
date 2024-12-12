07d49201250b6306b62acb8bf8e697d5047f6d91
L1mbxbx <ilya.msd2003@gmail.com>
2024-12-12 18:01:17 +0200

    Add Affinities for deployment.yml and statefulSet.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,3 +18,5 @@ kubectl apply -f .infrastructure/app/deployment.yml
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 # kubectl apply -f .infrastructure/ingress/ingress.yml
+
+kubectl taint nodes -1 app=mysql:NoSchedule
