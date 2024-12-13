5ae9195bc2e530aec21044b0101a7146b2166d30
nikita <niklafinskiy@gmail.com>
2024-12-13 16:48:59 +0100

    solved

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,5 +16,5 @@ kubectl apply -f .infrastructure/app/hpa.yml
 kubectl apply -f .infrastructure/app/deployment.yml
 
 # Install Ingress Controller
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 # kubectl apply -f .infrastructure/ingress/ingress.yml
